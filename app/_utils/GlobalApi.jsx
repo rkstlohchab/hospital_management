804014e42fef55ea2f7d427b2dd9492a52aaa3ec
Rakshit Kumar <rkstlohchab@gmail.com>
2024-06-10 10:39:13 +0530

    maybe working

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -3,12 +3,12 @@ const { default: axios } = require("axios");
 
 const API_KEY=process.env.NEXT_PUBLIC_STRAPI_API_KEY;
 
-const axiosClient=axios.create({
-    baseURL:'http://localhost:1337/api',
-    headers:{
-        'Authorization':`Bearer ${API_KEY}`
-    }
-})
+const axiosClient = axios.create({
+  baseURL: "https://strapi-hospital.onrender.com/api",
+  headers: {
+    Authorization: `Bearer ${API_KEY}`,
+  },
+});
 
  const getCategory=()=>axiosClient.get('/categories?populate=*');
 
